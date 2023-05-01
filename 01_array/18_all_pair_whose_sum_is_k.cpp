de70ac5155035c694332e6bbe4226ea5463902ce
ajay8106 <52490796+ajay8106@users.noreply.github.com>
2023-05-01 08:29:11 +0530

    Update 18_all_pair_whose_sum_is_k.cpp

--- 01_array/18_all_pair_whose_sum_is_k.cpp
+++ 01_array/18_all_pair_whose_sum_is_k.cpp
@@ -51,3 +51,36 @@ int getPairsCount(int arr[], int n, int sum)
 
     return count;
 }
+
+// More easy
+
+int getPairsCount(int arr[], int n, int k) {
+        sort(arr,arr+n);
+        int i=0;
+        int j=n-1;
+        int c=0;
+        while(i<j){
+            if(arr[i]+arr[j]==k){
+                c++;
+                int m=i+1;
+                while(m<j && (arr[m]+arr[j]) == k){
+                    c++;
+                    m++;
+                }
+                j--;
+            }
+            else if(arr[i]+arr[j] < k){
+                i++;
+            }
+            else if(arr[i]+arr[j] > k){
+                j--;
+            }
+        }
+        return c;
+    }
+
+
+
+
+
+
